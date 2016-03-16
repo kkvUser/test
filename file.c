1a8608e987cee8784090cb3b59beab9ae2cae61e
Nikolay Riskov <riskov@digimark.bg>
2016-03-16 17:21:02 +0200

    having fun with simple structs

--- file.c
+++ file.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Person{
+	char *name;
+	int age;
+} Person; 
+
+int Person_setName(Person *person, char *name){
+	person -> name = name;
+	return 0;
+}
+
+int Person_setAge(Person *person, int age){
+	person -> age = age;
+	return 0;
+}
+
+void Person_description(Person *person){
+	printf("[Person] %s, %i y.o. \n", person->name, person->age);
+}
+
 
 int main(int argc, char *argv[]){
 
 	printf("dont expect a fancy program here\n");	
+	Person *person = (Person *)malloc(sizeof(Person));
+	
+	Person_setName(person, "Gosho");
+	Person_setAge(person, 10);
+	Person_description(person);
+
 
 	return 0;
 }
